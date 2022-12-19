32bfce2aade8f30ea4b18e25aca378148a860035
nzoe <lorenzoyanga@gmail.com>
2022-12-19 20:09:47 +0800

    pgsql update

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
 
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
