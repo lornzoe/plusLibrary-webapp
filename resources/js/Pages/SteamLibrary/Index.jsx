5b13039781fa0a8e8865d21d9c12f35b186aaf83
nzoe <lorenzoyanga@gmail.com>
2022-12-22 22:01:39 +0800

    small page updates for index

--- resources/js/Pages/SteamLibrary/Index.jsx
+++ resources/js/Pages/SteamLibrary/Index.jsx
@@ -26,8 +26,8 @@ export default function Index({ auth, recentgames, lastupdate }) {
         header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Library</h2>}
         >
             <Head title="SteamLibrary" />
-            <div className="max-w-screen-2xl min-w-screen-2xl mx-auto p-4 sm:p-6 lg:p-8">
-                <div className="flex mt-4">
+            <div className="max-w-screen-2xl min-w-screen-2xl mx-auto pl-0 pr-0 p-2 lg:p-2 xl:p-8">
+                <div className="flex mt-4 pl-2 lg:p-0">
                     <a href="/refreshsteamlib">
                         <PrimaryButton processing={processing}>/refreshsteamlib</PrimaryButton>
                     </a>
@@ -35,7 +35,7 @@ export default function Index({ auth, recentgames, lastupdate }) {
                     <small className="ml-3 text-sm text-gray-400">// {dayjs(lastupdate ? lastupdate.updated_at : null).fromNow()}</small>
                 </div>
 
-                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <div className="mt-6 bg-white shadow-sm lg:rounded-lg divide-y">
                     {recentgames.map(
                         steamgame=>
                         <SteamGame key={steamgame.id} steamgame={steamgame}/>
