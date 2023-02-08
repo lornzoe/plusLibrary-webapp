7c092e81e420df8932f032702620c26b15a2be79
nzoe <lorenzoyanga@gmail.com>
2023-02-09 00:16:42 +0800

    attempted refactor of steamgamee

--- resources/js/Pages/SteamGame/Index.jsx
+++ resources/js/Pages/SteamGame/Index.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/inertia-react';
-import SteamGame from '@/Components/SteamGame'
+import SteamGame from '@/Components/SteamGame';
+import SteamGameV2 from '@/Components/SteamGameV2'
 
 export default function Index({auth, game}) {
 
@@ -11,12 +12,18 @@ export default function Index({auth, game}) {
             auth={auth}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{game.name}</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="GameView" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div className="flex-auto w-2xl mx-auto pl-0 pr-0 p-2 lg:p-2 xl:p-8">
+                <div className="mt-6 bg-white shadow-sm lg:rounded-lg divide-y p-2">
+                    <div className="m-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">new</div>
+                    </div>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">test</div>
+                        <SteamGameV2 key={game.id} steamgame={game}/>
+                    </div>
+                    <div className="m-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">orig</div>
                     </div>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <SteamGame key={game.id} steamgame={game}/>
